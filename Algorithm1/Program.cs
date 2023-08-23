13bff3c91f9b8f80486a75d1fe0557e0fa4bafef
ONYEDIKA ILODIBE <85747585+chrisilodibe@users.noreply.github.com>
2023-08-23 01:23:35 +0100

    done

---
+++ Algorithm1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Algorithm1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Words = "chisom is my name. he is from oba";
+
+            Dictionary<string,int> collect = new Dictionary<string,int>();
+
+            string[] words = Words.Split(" ");
+
+            foreach (string word in words)
+            {
+                if (collect.ContainsKey(word))
+                {
+                    int value = collect[word] + 1;
+                    collect[word] = value;
+                }
+                else
+                {
+                    collect.Add(word, 1);
+                }
+            }
+            foreach (var Value in collect)
+            {
+                Console.WriteLine($" {Value.Key} {Value.Value}");
+            }
+        }
+    }
+}
